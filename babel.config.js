dcc49ffa37f2a6e8b0b3ae55bfff705d4b3065b1
Budget It Developer <dev@budgetit.app>
2026-02-26 00:31:33 +0300

    fix: resolve import.meta blank page error in zustand web build

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'babel-plugin-transform-import-meta',
+      ['babel-plugin-transform-define', {
+        'import.meta.env.MODE': '"production"',
+        'import.meta.env': '{"MODE": "production"}'
+      }]
+    ],
   };
 };
